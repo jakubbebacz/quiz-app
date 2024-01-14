2555d3a67632b6d1e3df0147bbc5ff8d3ce37ce9
jakubbebacz <jakubbebacz0@gmail.com>
2024-01-14 13:06:51 +0100

    Fixed problem with enum

--- src/helpers/question-type.enum.ts
+++ src/helpers/question-type.enum.ts
@@ -1,6 +1,12 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum QuestionType {
   Single = 'single',
   Multiple = 'multiple',
   Sorting = 'sorting',
   PlainText = 'plainText',
 }
+
+registerEnumType(QuestionType, {
+  name: 'QuestionType',
+});
